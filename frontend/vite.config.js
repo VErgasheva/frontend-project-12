597a962fcf564d0ec77a3fbe503c41795743da55
VErgasheva <vikser587@gmail.ru>
2025-05-23 03:27:54 +0500

    1

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,10 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:5001'
-    }
-  }
+      '/api': 'http://localhost:5001',
+    },
+  },
+  build: {
+    outDir: 'dist',
+  },
 });
