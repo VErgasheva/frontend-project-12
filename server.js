b2b3c1c6f2f9fee340a45a4007d1e56b3b117bbe
VErgasheva <vikser587@gmail.ru>
2025-06-02 22:48:03 +0500

    7.1

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+import { createRequire } from 'module';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import express from 'express';
-import chatServer from '@hexlet/chat-server';
 
+const require = createRequire(import.meta.url);
+const chatServer = require('@hexlet/chat-server');
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
