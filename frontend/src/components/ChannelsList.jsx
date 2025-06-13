65963f59bb4f4f767d4d5f787cf4d9f3b1743d65
VErgasheva <vikser587@gmail.ru>
2025-06-13 20:00:56 +0500

    9.2

--- frontend/src/components/ChannelsList.jsx
+++ frontend/src/components/ChannelsList.jsx
@@ -1,13 +1,13 @@
 import { PlusSquareFill } from 'react-bootstrap-icons'
 import { Button, Col, Dropdown, ButtonGroup } from 'react-bootstrap'
-import { actions as channelActions } from '../slices/channelsSlice.js'
-import { useDispatch } from 'react-redux'
+import { actions } from '../slices/channelsSlice.js'
+import { useDispatch, useSelector } from 'react-redux'
 import { useTranslation } from 'react-i18next'
 
 function ChannelsList({ currentChannel, channels, showModal }) {
   const dispatch = useDispatch()
   const { t } = useTranslation()
-  const currentChannelId = currentChannel ? currentChannel.id : '1'
+  const selectedChannelId = useSelector(state => state.channels.selectedChannelId)
 
   return (
     <Col md={2} className="col-4 border-end px-0 bg-light flex-column h-100 d-flex">
@@ -19,34 +19,37 @@ function ChannelsList({ currentChannel, channels, showModal }) {
         </button>
       </div>
       <ul id="channels-box" className="nav flex-column nav-pills nav-fill px-2 mb-3 overflow-auto h-100 d-block">
-        {channels.map(channel => (
-          <li className="nav-item w-100" key={channel.id}>
-            <Dropdown as={ButtonGroup} className="d-flex">
-              <Button
-                className="w-100 rounded-0 text-start btn"
-                variant={channel.id === currentChannelId ? 'secondary' : undefined}
-                onClick={() => { dispatch(channelActions.selectChannel(channel.id)) }}
-              >
-                <span className="me-1">#</span>
-                {channel.name}
-              </Button>
-              {channel.removable && (
-                <>
-                  <Dropdown.Toggle
-                    split
-                    variant={channel.id === currentChannelId ? 'secondary' : undefined}
-                  >
-                    <span className="visually-hidden">{t('Channel management')}</span>
-                  </Dropdown.Toggle>
-                  <Dropdown.Menu>
-                    <Dropdown.Item onClick={() => showModal('removeChannel', channel)}>{t('Delete')}</Dropdown.Item>
-                    <Dropdown.Item onClick={() => showModal('renameChannel', channel)}>{t('Rename')}</Dropdown.Item>
-                  </Dropdown.Menu>
-                </>
-              )}
-            </Dropdown>
-          </li>
-        ))}
+        {channels.map(channel => {
+          const isSelected = String(channel.id) === String(selectedChannelId)
+          return (
+            <li className="nav-item w-100" key={channel.id}>
+              <Dropdown as={ButtonGroup} className="d-flex">
+                <Button
+                  className="w-100 rounded-0 text-start btn"
+                  variant={isSelected ? 'secondary' : undefined}
+                  onClick={() => { dispatch(actions.selectChannel(channel.id)) }}
+                >
+                  <span className="me-1">#</span>
+                  {channel.name}
+                </Button>
+                {channel.removable && (
+                  <>
+                    <Dropdown.Toggle
+                      split
+                      variant={isSelected ? 'secondary' : undefined}
+                    >
+                      <span className="visually-hidden">{t('Channel management')}</span>
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu>
+                      <Dropdown.Item onClick={() => showModal('removeChannel', channel)}>{t('Delete')}</Dropdown.Item>
+                      <Dropdown.Item onClick={() => showModal('renameChannel', channel)}>{t('Rename')}</Dropdown.Item>
+                    </Dropdown.Menu>
+                  </>
+                )}
+              </Dropdown>
+            </li>
+          )
+        })}
       </ul>
     </Col>
   )
