3f2cde820232523d1269ec9c473c952f4d2ccd1a
VErgasheva <vikser587@gmail.ru>
2025-06-14 22:11:18 +0500

    9.3

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,17 +1,19 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { io } from 'socket.io-client'
 import rootReducer from './slices/index.js'
 import createI18n from './i18n.js'
 import { I18nextProvider } from 'react-i18next'
 import App from './components/App.jsx'
-import { Provider, useSelector, useStore } from 'react-redux'
+import { Provider } from 'react-redux'
 import { configureStore } from '@reduxjs/toolkit'
 import { messagesApi } from './slices/messagesSlice.js'
 import { channelsApi, actions as channelsActions } from './slices/channelsSlice.js'
 import log from './logger.js'
 
 let socket = null
+
+const container = document.getElementById('chat')
 
 const setupSocket = (store) => {
   const token = localStorage.getItem('token')
@@ -22,15 +24,13 @@ const setupSocket = (store) => {
     }
     return
   }
-
-  if (socket && socket.auth && socket.auth.token === `Bearer ${token}` && socket.connected) {
+  if (socket && socket.auth && socket.auth.token === token && socket.connected) {
     return
   }
   if (socket) {
     socket.disconnect()
     socket = null
   }
-
   socket = io({
     auth: { token: `Bearer ${token}` },
   })
@@ -60,40 +60,34 @@ const setupSocket = (store) => {
       store.dispatch(channelsApi.util.invalidateTags(['Channels']))
     })
 }
-const store = configureStore({
-  reducer: rootReducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(messagesApi.middleware, channelsApi.middleware),
-})
 
-const SocketManager = ({ children }) => {
-  const store = useStore()
-  const isAuth = useSelector((state) => state.authUser.isAuth)
-
-  useEffect(() => {
-    setupSocket(store)
-    return () => {
+const renderApp = async () => {
+  const store = configureStore(rootReducer)
+  let prevAuth = Boolean(localStorage.getItem('token'))
+  setupSocket(store)
+  store.subscribe(() => {
+    const isAuthenticated = store.getState().user.isAuthenticated
+    const token = localStorage.getItem('token')
+    if (isAuthenticated && token) {
+      setupSocket(store)
+    } else {
       if (socket) {
         socket.disconnect()
         socket = null
       }
     }
-  }, [isAuth, store])
+    prevAuth = isAuthenticated
+  })
 
-  return children
+  const i18n = await createI18n()
+  const root = createRoot(container)
+  root.render(
+    <I18nextProvider i18n={i18n}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </I18nextProvider>
+  )
 }
 
-const i18n = createI18n()
-
-const container = document.getElementById('chat')
-const root = createRoot(container)
-
-root.render(
-  <Provider store={store}>
-    <I18nextProvider i18n={i18n}>
-      <SocketManager>
-        <App />
-      </SocketManager>
-    </I18nextProvider>
-  </Provider>
-)
+renderApp()
