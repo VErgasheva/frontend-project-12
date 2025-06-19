aae4334dd1923a06a6a20b06711254fd3f5f5915
VErgasheva <vikser587@gmail.ru>
2025-06-19 22:13:32 +0500

    9.6

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -2,9 +2,6 @@ import axios from 'axios'
 
 const axiosInstance = axios.create({
   baseURL: '/api/v1',
-  headers: {
-    'Content-Type': 'application/json',
-  },
 })
 
 axiosInstance.interceptors.request.use((config) => {
